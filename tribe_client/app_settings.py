1e78973f2d5999c7d3391c1379cf48890ddfcc8b
Rene A. Zelaya <4641983+rzelayafavila@users.noreply.github.com>
2016-02-12 14:22:31 -0500

    Changing app_settings.py to follow PEP8 guidelines and also got rid of the 'CROSSREF_DB' setting (as it was just a duplicate of CROSSREF).

--- tribe_client/app_settings.py
+++ tribe_client/app_settings.py
@@ -1,16 +1,13 @@
 from django.conf import settings
 
-TRIBE_URL = getattr(settings, 'TRIBE_URL', 'http://tribe.greenelab.com')
+TRIBE_URL = getattr(settings, 'TRIBE_URL', 'https://tribe.greenelab.com')
 
 ACCESS_CODE_URL = TRIBE_URL + "/oauth2/authorize/"
 ACCESS_TOKEN_URL = TRIBE_URL + "/oauth2/token/"
 
-ENTREZ = 'Entrez'
-
-CROSSREF_DB = '&xrdb=' + getattr(settings, 'TRIBE_CROSSREF_DB', ENTREZ)
-CROSSREF = getattr(settings, 'TRIBE_CROSSREF_DB', ENTREZ)
+CROSSREF = getattr(settings, 'TRIBE_CROSSREF_DB', 'Entrez')
 
 TRIBE_ID = getattr(settings, 'TRIBE_ID', '')
-TRIBE_SECRET= getattr(settings, 'TRIBE_SECRET', '')
+TRIBE_SECRET = getattr(settings, 'TRIBE_SECRET', '')
 
 TRIBE_REDIRECT_URI = getattr(settings, 'TRIBE_REDIRECT_URI', '')
