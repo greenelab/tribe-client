f5f2093a9644069dd1d511d0c10d74b4bc6840b9
Rene A. Zelaya <4641983+rzelayafavila@users.noreply.github.com>
2016-02-17 11:07:09 -0500

    Modifying line breaks in views.py to either do visual indents or do hanging indents with no parameters on the first line to comply with PEP8. Also adding a docstring for the 'create_geneset()' view, explaining the required format of the geneset data that gets posted.

--- tribe_client/views.py
+++ tribe_client/views.py
@@ -9,11 +9,11 @@ import json
 
 def connect_to_tribe(request):
     if 'tribe_token' not in request.session:
-        return render(request, 'establish_connection.html', {
-                'tribe_url': TRIBE_URL,
-                'access_code_url': ACCESS_CODE_URL,
-                'client_id': TRIBE_ID,
-                'scope': 'write'})
+        return render(request, 'establish_connection.html',
+                      {'tribe_url': TRIBE_URL,
+                       'access_code_url': ACCESS_CODE_URL,
+                       'client_id': TRIBE_ID,
+                       'scope': 'write'})
     else:
         return display_genesets(request)
 
@@ -41,12 +41,13 @@ def display_genesets(request):
             return connect_to_tribe(request)
 
         else:  # The user must be logged in and has access to her/himself
-            genesets = utils.retrieve_user_genesets(access_token, {
-                    'full_genes': 'true', 'limit': 100})
+            genesets = utils.retrieve_user_genesets(
+                access_token, {'full_genes': 'true', 'limit': 100})
             tribe_user = get_user
-            return render(request, 'display_genesets.html', {
-                'tribe_url': TRIBE_URL, 'genesets': genesets,
-                'tribe_user': tribe_user})
+            return render(request, 'display_genesets.html',
+                          {'tribe_url': TRIBE_URL,
+                           'genesets': genesets,
+                           'tribe_user': tribe_user})
 
     else:
         return connect_to_tribe(request)
@@ -68,8 +69,8 @@ def display_versions(request, geneset):
                 for annotation in version['annotations']:
                     version['gene_list'].append(
                             annotation['gene']['standard_name'])
-            return render(request, 'display_versions.html', {
-                    'versions': versions})
+            return render(request, 'display_versions.html',
+                          {'versions': versions})
 
 
 def return_access_token(request):
@@ -82,18 +83,56 @@ def return_access_token(request):
 
 
 def create_geneset(request):
+    """
+    View to handle the creation of genesets on Tribe when users make
+    POST request to the '/tribe_client/create_geneset' URL.
 
-    geneset_info = request.POST.get('geneset')
-    geneset_info = json.loads(geneset_info)
-    geneset_info['xrdb'] = CROSSREF
+    Arguments:
+    request -- Request object, which contains a dictionary-like object
+    of POST data, among other things.
 
+    * In the POST data, there should be a 'geneset' object that contains
+    the information for the geneset being created. This 'geneset' is
+    passed in the POST data as a string, which we load as json to get a
+    dictionary. The general format for the data in this geneset object is:
+
+    geneset = {
+        organism: 'Mus musculus',  # Required
+        title: 'Sample title',  # Required
+
+        abstract: 'Sample abstract',  # Optional
+
+        # Genes to be included in the geneset are sent in the 'annotations'
+        # dictionary, and this whole dictionary is optional. The geneset
+        # can have as many or as few annotations as desired. The format for
+        # the annotations dictionary is:
+        # {gene_id1: [<list of pubmed ids associated with that gene>],
+        #  gene_id2: [<list of pubmed ids associated with that gene>]...}
+        # The type of identifier for the gene_ids is whatever is set
+        # in the CROSSREF setting.
+        annotations: {55982: [20671152, 19583951],
+                      18091: [8887666], 67087: [],
+                      22410:[]}
+    }
+
+    Returns:
+    Either -
+
+    a) The Tribe URL of the geneset that has just been created, or
+    b) A 401 Unauthorized response if the user is not signed in
+
+    """
     if 'tribe_token' in request.session:
         tribe_token = request.session['tribe_token']
         is_token_valid = utils.retrieve_user_object(tribe_token)
+
         if (is_token_valid == 'OAuth Token expired'):
-            tribe_token = None
-            tribe_response = {'response': 'Not Authorized'}
+            request.session.clear()
+            return connect_to_tribe(request)
         else:
+            geneset_info = request.POST.get('geneset')
+            geneset_info = json.loads(geneset_info)
+            geneset_info['xrdb'] = CROSSREF
             tribe_response = utils.create_remote_geneset(tribe_token,
                                                          geneset_info)
             slug = tribe_response['slug']
@@ -101,14 +140,11 @@ def create_geneset(request):
 
             geneset_url = TRIBE_URL + "/#/use/detail/" + creator + "/" + slug
             tribe_response = {'geneset_url': geneset_url}
+            json_response = json.dumps(tribe_response)
+            return HttpResponse(json_response, content_type='application/json')
 
     else:
-        tribe_token = None
-        tribe_response = {'response': 'Not Authorized'}
-
-    json_response = json.dumps(tribe_response)
-
-    return HttpResponse(json_response, content_type='application/json')
+        return HttpResponse('Unauthorized', status=401)
 
 
 def return_user_obj(request):
