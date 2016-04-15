0c26813cdd4eec47d54ceba751e715d1184fe891
Rene A. Zelaya <4641983+rzelayafavila@users.noreply.github.com>
2016-04-15 12:21:08 -0400

    Modifying app_settings.py so it uses try...except to import django settings, and defines some default settings in case django is not installed in Python environment.

--- tribe_client/app_settings.py
+++ tribe_client/app_settings.py
@@ -1,13 +1,20 @@
-from django.conf import settings
+TRIBE_URL = 'https://tribe.greenelab.com'
+CROSSREF = 'Entrez'
 
-TRIBE_URL = getattr(settings, 'TRIBE_URL', 'https://tribe.greenelab.com')
+try:
+    from django.conf import settings
+
+    TRIBE_URL = getattr(settings, 'TRIBE_URL', TRIBE_URL)
+
+    CROSSREF = getattr(settings, 'TRIBE_CROSSREF_DB', CROSSREF)
+
+    TRIBE_ID = getattr(settings, 'TRIBE_ID', '')
+    TRIBE_SECRET = getattr(settings, 'TRIBE_SECRET', '')
+
+    TRIBE_REDIRECT_URI = getattr(settings, 'TRIBE_REDIRECT_URI', '')
+
+except ImportError:
+    pass
 
 ACCESS_CODE_URL = TRIBE_URL + "/oauth2/authorize/"
 ACCESS_TOKEN_URL = TRIBE_URL + "/oauth2/token/"
-
-CROSSREF = getattr(settings, 'TRIBE_CROSSREF_DB', 'Entrez')
-
-TRIBE_ID = getattr(settings, 'TRIBE_ID', '')
-TRIBE_SECRET = getattr(settings, 'TRIBE_SECRET', '')
-
-TRIBE_REDIRECT_URI = getattr(settings, 'TRIBE_REDIRECT_URI', '')
